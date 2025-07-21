621040bd9b83079ddf83a1335a8b214b55744e54
Trần Trân Lộc <tran26122003@gmail.com>
2025-07-21 16:15:52 +0700

    Crud Theater

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse, RedirectResponse
 from app.utils.response import error_response
-from app.api.v1 import auth, movies, users
+from app.api.v1 import auth, movies, theater, users
 # from app.core.database import Base, engine
 
 
@@ -12,7 +12,7 @@ app = FastAPI(title="Cinema Booking API", version="1.0.0")
 app.include_router(movies.router, prefix="/api/v1", tags=["Movies"])
 app.include_router(users.router,  prefix="/api/v1",tags=["Users"])
 app.include_router(auth.router,  prefix="/api/v1",tags=["Auth"])
-
+app.include_router(theater.router,  prefix="/api/v1",tags=["Theater"])
 @app.get("/")
 async def root():
     return RedirectResponse(url="/docs")
