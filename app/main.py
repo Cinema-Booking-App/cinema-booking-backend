37d9a77b64b9f4c149af24e26bb1f73c45b280f8
PhamVanTinh273 <tinh101334@donga.edu.vn>
2025-07-23 22:24:04 +0700

    promotions with crud

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse, RedirectResponse
 from app.utils.response import error_response
-from app.api.v1 import auth, movies, rooms, seat_layouts, seat_types, theaters, users
+from app.api.v1 import auth, movies, rooms, seat_layouts, seat_types, theaters, users, promotions
 # from app.core.database import Base, engine
 
 
@@ -16,6 +16,7 @@ app.include_router(theaters.router,  prefix="/api/v1",tags=["Theaters"])
 app.include_router(seat_types.router,  prefix="/api/v1",tags=["Seat Types"])
 app.include_router(seat_layouts.router,  prefix="/api/v1",tags=["Seat Layouts"])
 app.include_router(rooms.router,  prefix="/api/v1",tags=["Rooms"])
+app.include_router(promotions.router, prefix="/api/v1", tags=["Promotions"])
 
 @app.get("/")
 async def root():
