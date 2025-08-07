6629a78d3959830b6aaa89d5460606fed4278194
Trần Trân Lộc <tran26122003@gmail.com>
2025-08-07 18:47:00 +0700

    refactor get_current_active_user function and adjust dependency usage in movies endpoint

--- app/core/security.py
+++ app/core/security.py
@@ -3,9 +3,7 @@ from app.models.users import UserStatusEnum, Users
 from app.services.auth_service import get_current_user
 
 # Hàm get_current_active_user vẫn ở đây
-def get_current_active_user(current_user: Users = Depends(get_current_user)):
-    
-    """Lấy người dùng hiện tại chỉ khi họ đã được kích hoạt."""
+def get_current_active_user(current_user = Depends(get_current_user)): 
     if current_user.status != UserStatusEnum.active:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, detail="Tài khoản chưa được xác minh"
